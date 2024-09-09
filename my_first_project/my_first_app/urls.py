0cf38c6e2c89f14afd0abb5c5078ff6b53f50c1c
ronseriajorge <ronseriajorge@gmail.com>
2024-09-09 18:19:53 -0500

    se crean las urls independeintes

--- my_first_project/my_first_app/urls.py
+++ my_first_project/my_first_app/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.http import HttpResponse
+from django.urls import path
+
+def my_view (request, *args, **kwargs):
+    print(args)
+    print(kwargs)
+    return HttpResponse("")
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("carros/", include("my_first_app.urls")),
+    path("listado/",my_view),
+    path("detalle/<int:id>",my_view)
 ]
---
+++ my_first_project/my_first_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def my_view(request):
+    car_list =[
+        {"title":"BMW"},
+        {"title":"Mazda"}
+    ]
+    context = {
+        "car_list": car_list
+    }
+    return render(request,"my_first_app/car_list.html",context)
