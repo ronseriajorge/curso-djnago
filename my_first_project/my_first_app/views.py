1263b1dca1ed985a7dc1b20ae9dd2df56759dbb1
ronseriajorge <ronseriajorge@gmail.com>
2024-09-10 15:04:57 -0500

    Se ajustan las vistas y se crea una como Clase

--- my_first_project/my_first_app/views.py
+++ my_first_project/my_first_app/views.py
@@ -1,4 +1,7 @@
+from django import views
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 def my_view(request):
@@ -10,3 +13,20 @@ def my_view(request):
         "car_list": car_list
     }
     return render(request,"my_first_app/car_list.html",context)
+
+class CarListView (TemplateView):
+    template_name = "my_first_app/car_list.html"
+
+    def get_context_data(self):
+        car_list =[
+            {"title":"BMW"},
+            {"title":"Mazda"}
+        ]
+        return {
+            "car_list": car_list
+        }
+
+def my_test_view (request, *args, **kwargs):
+    print(args)
+    print(kwargs)
+    return HttpResponse("")
